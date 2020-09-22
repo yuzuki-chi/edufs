6f2c87766924dbe885dae1002ff8bccd442eec69
yuzuki-chi <c011719086@edu.teu.ac.jp>
2020-09-22 21:51:08 +0900

    ひとまず

--- src/FileSystem/Dentry.java
+++ src/FileSystem/Dentry.java
@@ -1,8 +1,73 @@
 package FileSystem;
 
-public interface Dentry {
+import FileObject.Directory;
+
+public class Dentry {
     /**
      * dエントリオブジェクト (dentry object)
      *  ディレクトリエントリと, 対応するファイルとのリンクについて情報を保存するオブジェクト.
      */
+
+    AtomicT dCount;     //dエントリオブジェクト利用カウンタ
+    int dFlags;         //dエントリのフラグ
+    Inode dInode;       //ファイル名に対応するinode
+    Dentry dParent;     //親ディレクトリのdエントリオブジェクト
+    ListHead dHash;     //ハッシュテーブルエントリ中のリストへのポインタ
+    ListHead dLru;      //未使用dエントリリストへのポインタ
+    ListHead dChild;    //同じ親ディレクトリを持つdエントリオブジェクトをリストにするためのポインタ
+    ListHead dSubdirs;  //親ディレクトリのdエントリオブジェクトが, その子のdエントリオブジェクトリストを指すためのポインタ
+    ListHead dAlias;    //同じinodeを共有するdエントリオブジェクトのリスト (エイリアス, 別名)
+    int dMounted;       //dエントリがファイルシステムのマウントポイントの場合, このフラグを1にする
+    Qstr dName;         //ファイル名
+    long dTime;         //dRevalidateメソッドによって使用される
+    DentryOperations dOp;   //dエントリのメソッド群
+    SuperBlock dSb;     //ファイルのスーパーブロックオブジェクト
+    long dVfsFlags;     //dエントリキャッシュのフラグ群
+    //void dFsdata;       //ファイルシステム依存のデータ
+    String dIname;      //短いファイル名を記録する領域
+
+    int dRevalidate(Dentry dentry, int flag) {
+        /* ファイルのパス名の変換に使用する前に, dエントリオブジェクトが, まだ有効であるかどうかを調べる.
+           デフォルトのVFS関数では何も行わない. (ネットワークファイルシステムが固有の関数を持つ)
+         */
+        return 0;
+    }
+
+    int dHash(Dentry dentry, String name) {
+        /* ハッシュ値を作成する. この関数はdエントリハッシュテーブルで利用するファイルシステム独自のハッシュ関数.
+           引数dentryは, パス名要素が入っているディレクトリを指定する.
+           引数nameは, 検索するべきパス名の要素と, ハッシュ関数によって生成された値との両方を含む構造体を指す.
+         */
+        return 0;
+    }
+
+    int dCompare(Directory dir, String name1, String name2) {
+        /* 2つのファイル名を比較する.
+           name1は, dirによって参照されるディレクトリに属していなければならない.
+           デフォルトのVFS関数では, 普通の文字列比較を行う.
+           (各ファイルシステムでは独自でこの方法を実装していて, 例えばMS-DOSでは大文字と小文字を区別しない)
+         */
+        return 0;
+    }
+
+    int dDelete(Dentry dentry) {
+        /* dエントリオブジェクトへの最後の参照が削除された時に呼び出される (dCountが0になったとき)
+           デフォルトのVFS関数では何も行わない.
+         */
+        return 0;
+    }
+
+    int dRelease(Dentry dentry) {
+        /* dエントリオブジェクトが解放される時に呼び出される (スラブアロケータへの解放)
+           デフォルトのVFS関数では何も行わない.
+         */
+        return 0;
+    }
+
+    int dIput(Dentry dentry, Inode ino) {
+        /* dエントリオブジェクトが「負」になると呼び出される. (対応するinodeが無くなった時)
+           デフォルトのVFS関数では, inodeオブジェクトを解放するためにiput()を呼び出す.
+         */
+        return 0;
+    }
 }
--- src/FileSystem/File.java
+++ src/FileSystem/File.java
@@ -18,7 +18,7 @@ public class File {
     AtomicT fCount;     //ファイルオブジェクトの利用カウンタ
     int fFlags;         //ファイルのオープン時に指定されたフラグ
     modeT fMode;        //プロセスのアクセスモード
-    loffT fPos;         //現在使用しているファイルのオフセット (ファイルポインタ)
+    LoffT fPos;         //現在使用しているファイルのオフセット (ファイルポインタ)
     long fReada;        //先読みフラグ
     long fRamax;        //先読みする最大ページ数
     long fRaend;        //最後の先読みのあとのファイルポインタ
@@ -32,4 +32,107 @@ public class File {
     //void privateData;   //端末ドライバ用
     Kiobuf fIobuf;      //ダイレクトアクセスバッファ用のディスクリプタ
     long fIobufLock;    //ダイレクトI/O用のロック
+
+    int llseek(File file, LoffT offset, int origin) {
+        /* ファイルポインタを更新する */
+        return 0;
+    }
+
+    int read(File file, Buffer buf, SizeT count, LoffT offset) {
+        /* offsetで指定される一から開始し, countで指定されるバイト数をファイルから読み取る.
+           その後, (通常ファイルポインタである)*offsetの値を増やす
+         */
+        return 0;
+    }
+
+    int write(File file, Buffer buf, SizeT count, LoffT offset) {
+        /* offsetで指定される一から開始し, countバイト分をファイルに書き込む.
+           その後, (通常ファイルポインタである)*offsetの値を増やす
+         */
+        return 0;
+    }
+
+    int readdir(Inode dir, Dentry dirent, Inode filldir) { //filldirがよくわからない
+        /* ディレクトリの次のディレクトリエントリをdirentに返す.
+           filldir引数には, ディレクトリエントリ中のメンバを取り出す補助関数のアドレスが入る
+         */
+        return 0;
+    }
+
+    int poll(File file, PollTable pollTable) {
+        /* ファイルに変更が行われているかどうかを確認し, 行わなければ行われるまで休止する */
+        return 0;
+    }
+
+    int ioctl(Inode inode, File file, int cmd, String ... arg) {
+        /* 主にデバイスファイルに適用され, 対応するハードウェアデバイスに命令を送る.
+           通常ファイルやソケットファイルにも定義される.
+         */
+        return 0;
+    }
+
+    int mmap(File file, VmArea vma) {
+        /* プロセスアドレス空間へ, ファイルのメモリマッピングを行う */
+        return 0;
+    }
+
+    int open(Inode inode, File file) {
+        /* 新しいファイルオブジェクトを生成し, 対応するinodeオブジェクトとリンクすることにより, ファイルをオープンする.*/
+        return 0;
+    }
+
+    int flush(File file) {
+        /* オープンファイルへの参照がクローズされた時 (ファイルオブジェクトのfCountが減った時) に呼び出される.
+           実際の用途は, ファイルシステムによって異なる.
+         */
+        return 0;
+    }
+
+    int release(Inode inode, File file) {
+        /* ファイルオブジェクトを解放する. オープンファイルへの最後の参照がクローズされた時
+         (ファイルオブジェクトのfCountが0になった時)に呼び出される.
+         */
+        return 0;
+    }
+
+    int fsync(File file, Dentry dentry) {
+        /* ファイルのキャッシュされたデータのすべてをディスクに書き込む */
+        return 0;
+    }
+
+    int fasync(int fd, File file, int on) {
+        /* シグナルによる非同期I/Oの通知を許可または禁止する */
+        return 0;
+    }
+
+    int lock(File file, int cmd, FileLock fileLock) {
+        /* ファイルをロックする */
+        return 0;
+    }
+
+    int readv(File file, Buffer vector, int count , LoffT offset) {
+        /* ファイルからバイト列を読み込み, vectorで示したバッファに結果を入れる.
+           バッファの数はcountで指定する.
+         */
+        return 0;
+    }
+
+    int writev(File file, Buffer vector, int count, LoffT offset) {
+        /* vectorで示すバッファ内のバイト列をファイルに書き込む.
+           バッファの数はcountで指定する.
+         */
+        return 0;
+    }
+
+    int sendpage(File file, LoffT offset, int size, Pointer pointer, Fill fill) { //わからん
+        /* あるファイルから別のファイルにデータを転送する. ソケットで使用する */
+        return 0;
+    }
+
+    long getUnmappedArea(File file, long addr, long len, long offset, long flags) {
+        /* ファイルをマップするために, 未使用アドレスの範囲を取得する.
+           (フレームバッファのメモリマッピングで使用する)
+         */
+        return 0;
+    }
 }
